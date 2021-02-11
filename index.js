ae06397b1f5e4a1acb2717d3221c0bf453b2ff0c
vahe2002 <florencia73000@gmail.com>
2021-02-11 11:04:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const buttons = document.querySelectorAll("button");
     function () {
       //Lägg in och fyll datalagret vid checkout
 
+     window.dataLayer = window.dataLayer || [];
+     dataLayer.push({
+     event:"Checkout",
+     product:button.getAttribute("data-product")
+      });
+
+
       window.location =
         "thankyou.html?product=" +
         button.getAttribute("data-product") +
